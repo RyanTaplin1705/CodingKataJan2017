34d12bce5bc4a95d655e82f5132af0f73bf21dbe
Thomas Heslin <tjheslin1@gmail.com>
2017-01-18 15:54:06 +0000

    Tom - Updating main with message

--- src/main/java/io/github/ryantaplin1705/Main.java
+++ src/main/java/io/github/ryantaplin1705/Main.java
@@ -3,6 +3,6 @@ package io.github.ryantaplin1705;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello Tom :O");
+        System.out.print("Hello Tom :O!!!");
     }
 }
